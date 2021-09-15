529fb282b95879dc1e3aac2ce8f3791c4737f47e
pzyv5hsfv5u5kus9 <pzyv5hsfv5u5kus9@gmail.com>
2021-09-15 19:02:34 -0400

    intial knn code dump

--- src/main/java/edu/brown/cs/student/main/Stars.java
+++ src/main/java/edu/brown/cs/student/main/Stars.java
@@ -2,30 +2,48 @@ package edu.brown.cs.student.main;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
+
+
 public class Stars {
-  private List<List<String>> dataset;
-  private HashMap<String, double[]> positions;
+  private List<List<String>> dataset = new ArrayList<>();
+  private HashMap<String, double[]> positions = new HashMap<>();
 
   /**
    * Reads a csv file and stores it into both dataset and positions.
    * @param file - csv file to read into the database
    */
   public void storeStars(String file) {
-    // TODO: fill in database/positions initialization/data ingression
     try {
       BufferedReader buffer = new BufferedReader(new FileReader(file));
       String l;
       while ((l = buffer.readLine()) != null) {
         String[] star = l.split(",");
-        dataset.add(Arrays.asList(star));
+        if (star.length == 5) {
+          this.dataset.add(Arrays.asList(star));
+        } else {
+          throw new Exception("ERROR: incorrectly formatted data");
+        }
       }
     } catch (Exception e) {
       e.printStackTrace();
-      System.out.println("ERROR: file ingestion error");
+      System.out.println("ERROR: " + e);
+    }
+
+    for (List<String> star : this.dataset) {
+      try {
+        positions.put(star.get(1), new double[] {Double.parseDouble(star.get(2)),
+            Double.parseDouble(star.get(2)),
+            Double.parseDouble(star.get(2))});
+      } catch (Exception e) {
+        e.printStackTrace();
+        System.out.println("ERROR: star position not a valid number");
+      }
     }
   }
 
@@ -36,7 +54,7 @@ public class Stars {
    */
   public double[] getPosition(String name) {
     try {
-      return positions.get(name);
+      return this.positions.get(name);
     } catch (Exception e) {
       e.printStackTrace();
       System.out.print("ERROR: index key not in positions hashmap");
@@ -50,9 +68,28 @@ public class Stars {
    * @param pos - starting position
    * @return list of k star IDs that are closest to position
    */
-  public int[] getNeighborsFromPosition(int k, double[] pos) {
-    // TODO: fill out KNN
-    return null;
+  public List<Integer> getNeighborsFromPosition(int k, double[] pos) {
+    List<Integer> neighbors = new ArrayList<>();
+    List<List<Double>> distances = new ArrayList<>();
+    for (List<String> star : this.dataset) {
+      List<Double> pair = new ArrayList<>();
+      pair.add(Double.parseDouble(star.get(0)));
+      double distance = getEuclideanDistance(pos, getPosition(star.get(1)));
+      pair.add(distance);
+      distances.add(pair);
+    }
+    Comparator<List<Double>> comparator = (l1, l2) -> {
+      if (l1.get(1) > l2.get(1)) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+    distances.sort(comparator);
+    for (int i = 0; i < k; i++) {
+      neighbors.add(distances.get(i).get(1).intValue());
+    }
+    return neighbors;
   }
 
   /**
@@ -61,8 +98,14 @@ public class Stars {
    * @param name - name of star to search around
    * @return - list of k star IDs that are closest to the given star
    */
-  public int[] getNeighborsFromStar(int k, String name) {
+  public List<Integer> getNeighborsFromStar(int k, String name) {
     double[] pos = getPosition(name);
     return getNeighborsFromPosition(k, pos);
+  }
+
+  public double getEuclideanDistance(double[] a, double[] b) {
+    return Math.sqrt(Math.pow((a[0] - b[0]), 2)
+        + Math.pow((a[1] - b[1]), 2)
+        + Math.pow((a[2] - b[2]), 2));
   }
 }
