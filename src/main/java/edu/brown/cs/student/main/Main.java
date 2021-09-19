11205377df5db83916e1e2e941d299633fd595e4
pzyv5hsfv5u5kus9 <pzyv5hsfv5u5kus9@gmail.com>
2021-09-19 11:44:57 -0400

    test: system testing

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -130,6 +131,7 @@ public final class Main {
                     double y = Double.parseDouble(arguments[3]);
                     double z = Double.parseDouble(arguments[4]);
                     double[] pos = new double[] {x, y, z};
+                    System.out.println(Arrays.toString(pos)); // test ;
                     List<Integer> ids = star.getNeighborsFromPosition(k, pos);
                     ids.forEach(System.out::println);
                   } catch (Exception e) {
