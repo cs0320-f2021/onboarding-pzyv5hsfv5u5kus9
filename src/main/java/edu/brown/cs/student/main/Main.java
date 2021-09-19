23c1f9a1aefe6fca83a507955f6ff17edcc049ce
pzyv5hsfv5u5kus9 <pzyv5hsfv5u5kus9@gmail.com>
2021-09-19 14:35:51 -0400

    test: system testing

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -139,7 +140,7 @@ public final class Main {
                   break;
                 default:
                   System.out.println("ERROR: command structure for naive_neighbors,received: "
-                      + arguments.toString());
+                      + Arrays.toString(arguments));
                   break;
               }
               break;
