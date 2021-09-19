c75607046e455015015e5e0966b5968c710582be
pzyv5hsfv5u5kus9 <pzyv5hsfv5u5kus9@gmail.com>
2021-09-19 14:55:01 -0400

    test: system testing

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -69,7 +69,7 @@ public final class Main {
       while ((input = br.readLine()) != null) {
         try {
           input = input.trim();
-          String[] arguments = input.split("[^\\s\"']+|\"([^\"]*)\"|'([^']*)");
+          String[] arguments = input.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
           System.out.println(arguments.toString());
           int numArgs = arguments.length - 1;
           switch (arguments[0]) {
